38942c1da4ea6bbd13ed60689a4ed47a39c10112
bsd_witch <77200446+unix-witch@users.noreply.github.com>
2022-02-02 10:10:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+#https://discord.com/api/oauth2/authorize?client_id=937810355206357062&permissions=8&scope=bot
+
 import os 
+import random
 import discord
 from discord.ext import commands
 from keepalive import keepAlive
@@ -11,27 +14,52 @@ intents.members = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
 
+messages = [
+    "Stop playing league of legends",
+    "Take a shower",
+    "Among us??? Sussy??? Imposter league of legends?"
+]
+
+
 
 @client.event
 async def on_ready():
     print("Ready!")
+    print(client.guilds)
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(client.guilds)} sussy bakers"))
+    members = 0
+    
+    for guild in client.guilds:
+        for member in guild.members:
+            members += 1
+
+    with open("members.txt", "w") as f:
+        f.write(str(members))
+
 
 
 
 @client.event
 async def on_member_update(before, after):
+    #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=random.choice(status)))
+
 
     if after.activity != None:
-        print(len(after.activities))
         print(after.name)
         if len(after.activities) > 1:
-            if str(after.activities[1]).lower() == "league of legends":
+            print(after.activities[1].name)
+            if str(after.activities[1].name).lower() == "league of legends":
                 print("banning")
                 try:
-                    await after.ban()
+                    with open("hall-of-shame.txt", "a+") as f:
+                        f.write(after.name)
+
+
+                    await after.send(random.choice(messages))
+                    await after.ban(reason='Playing League of legends')
                 except discord.errors.Forbidden:
                     print("Not valid permissions")
+                    after.send("")
 
                 print(after.activities[1])
 
@@ -40,4 +68,4 @@ async def on_member_update(before, after):
 
 
 keepAlive()
-client.run(os.environ['TOKEN'])
+client.run(os.environ['TOKEN']) 
